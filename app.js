c07f75c0de3deb3688d841dc6cb643f54596927c
Yevhenii Merzhynskyi <yevhenii_merzhynskyi@epam.com>
2026-09-23 13:28:14 +0300

    Add prop in object from dev2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const user = {
   username: "anonim",
   role: "admin",
+  skill: ['js','ts', 'trello', 'jenkins', 'CI/CD'],
   login: function () {
     let message = "";
     message = this.username + "logged in";
